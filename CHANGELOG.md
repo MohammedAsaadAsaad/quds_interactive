d9158b21d6fc22a4db7924c1e4df4c5b9673103f
MohammedAsaadAsaad <m.a.asaad@hotmail.com>
2021-05-30 13:13:21 +0300

    updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
-## 0.0.1-beta
+## 0.0.1
 * First beta version
+
+
+## 0.0.1+1
+* Fixed some issues
--- lib/quds_interactive.dart
+++ lib/quds_interactive.dart
@@ -3,3 +3,6 @@ library quds_interactive;
 export 'quds_translation/internals.dart';
 export 'quds_theme/internals.dart';
 export 'quds_interactive/quds_interactive_app.dart';
+
+export 'package:quds_provider/quds_provider.dart';
+export 'package:quds_ui_kit/quds_ui_kit.dart';
